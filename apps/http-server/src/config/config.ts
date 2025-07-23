fde249e3db8474b2e76757d5f9beeb8db084ffd1
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-07-23 16:17:21 +0530

    Google OAUTH implemented

--- apps/http-server/src/config/config.ts
+++ apps/http-server/src/config/config.ts
@@ -3,5 +3,10 @@ config();
 
 export const PORT = process.env.PORT || 3002;
 export const JWT_USER_SECRET = process.env.JWT_USER_SECRET as string;
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+
+// For Google Oauth
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 export const OTP_SENDER_MAIL = process.env.OTP_SENDER_MAIL;
--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,9 +1,14 @@
+import './services/passport';
+import passport from 'passport';
+import session from 'express-session';
+import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
 import {
     PORT
 } from "./config/config";
 import { AuthRouter } from "./routes/authRoutes";
+import { OauthRouter } from './controllers/oauthControllers';
 
 const app = express();
 
@@ -15,10 +20,36 @@ const corsOptions = {
     optionsSuccessStatus: 200
 };
 
+app.use(cookieParser());
 app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
 app.use(cors(corsOptions));
 
-app.use("/api/v1/auth" , AuthRouter)
+// Session configuration
+app.use(
+    session({
+        // secret: process.env.SESSION_SECRET || 'defaultsecret',
+        secret: process.env.SESSION_SECRET as string,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 24 * 60 * 60 * 1000, // 1 day
+            secure: process.env.NODE_ENV === 'production',
+            httpOnly: true,
+        },
+    })
+);
+
+// Passport middlewares
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use("/api/v1/auth", AuthRouter);
+app.use("/auth", OauthRouter);
 
 
 app.get("/", (req, res) => {
